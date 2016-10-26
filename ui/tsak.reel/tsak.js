5438e3535558dc181b5ae97d1cccf38c8b64a23c
Alexei Grigoriev <alexgg@inbox.ru>
2016-10-26 11:11:41 +0000

    Add component Tsak

---
+++ ui/tsak.reel/tsak.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/tsak.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Tsak
+ * @extends Component
+ */
+exports.Tsak = Component.specialize(/** @lends Tsak# */ {
+    constructor: {
+        value: function Tsak() {
+            this.super();
+        }
+    }
+});
